777c2f6d13f29c62a9c4e853c25b22e3d794a9fe
unknown <gautam.b@gadgeon.com>
2020-12-26 22:12:01 +0530

    Customization and Validation

--- pseq/server.js
+++ pseq/server.js
@@ -9,7 +9,10 @@ const connection = new Sequelize('db','user','pass',{
     host:'localhost',
     dialect:'sqlite',
     storage:'db.sqlite',
-    operatorsAliases:false
+    operatorsAliases:false,
+    define:{
+        freezeTableName: true
+    }
 });
 
 const User = connection.define('User',{
@@ -18,8 +21,26 @@ const User = connection.define('User',{
         primaryKey: true,
         defaultValue: UUIDV4
     },
-    name: Sequelize.STRING,
-    bio: Sequelize.TEXT
+    first: Sequelize.STRING,
+    last: Sequelize.STRING,
+    full_name: Sequelize.STRING,
+    bio: Sequelize.TEXT},
+    {
+        hooks: {
+            beforeValidate: () => {
+                console.log('before validate');
+            },
+            afterValidate: () => {
+                console.log('after validate');
+            },
+            beforeCreate: (user) => {
+                user.full_name = `${user.first} ${user.last}`;
+                console.log('before create');
+            },
+            afterCreate: () => {
+                console.log('after create');
+            },
+        }
 });
 
 connection.sync({
@@ -27,7 +48,8 @@ connection.sync({
     force: true
 }).then(() => {
     User.create({
-        name: 'Joe',
+        first: 'Joe',
+        last: 'Smith',
         bio: 'New bio entry'
     })
 }).then(() => {
